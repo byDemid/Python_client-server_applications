255b26a063cacb3a1796f464c1b7cf73aa3f22ac
Demid <bydimko@yandex.ru>
2022-01-31 21:25:13 +0300

    modified client.py and server.py

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 """Программа-сервер"""
-
+import argparse
 import socket
 import sys
 import json
+import logging
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
     PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, IP_ADDRESS, PORT
 from common.utils import get_message, send_message
+from errors import IncorrectDataRecivedError
+from logs import server_log_config
+
+# Инициализация логирования сервера.
+SERVER_LOGGER = logging.getLogger('server')
 
 
 def process_client_message(message):
@@ -26,23 +32,35 @@ def process_client_message(message):
     }
 
 
+def create_arg_parser():
+    """
+    Парсер аргументов коммандной строки
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    return parser
+
+
 def main():
     '''
     Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
     :return:
     '''
+    parser = create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
+    # проверка получения корретного номера порта для работы сервера.
+    if not 1023 < listen_port < 65536:
+        SERVER_LOGGER.critical(
+            f'{listen_port} В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
+        sys.exit(1)
 
-    try:
-        listen_port = int(sys.argv[sys.argv.index(PORT) + 1]) if PORT in sys.argv else DEFAULT_PORT
-        if listen_port < 1024 or listen_port > 65535:
-            raise ValueError
-    except IndexError:
-        print('После параметра -\'p\' необходимо указать номер порта.')
-        sys.exit(1)
-    except ValueError:
-        print(
-            'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
-        sys.exit(1)
+    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
+                       f'адрес с которого принимаются подключения: {listen_address}. '
+                       f'Если адрес не указан, принимаются соединения с любых адресов.')
 
     # Загружаем какой адрес слушать
 
@@ -62,15 +80,22 @@ def main():
 
     while True:
         client, client_ip = transport.accept()
-        print(client_ip)
+        SERVER_LOGGER.info(f'Установлено соедение с ПК {client_ip}')
         try:
             message_from_client = get_message(client)
-            print(message_from_client)
+            SERVER_LOGGER.debug(f'Получено сообщение {message_from_client}')
             response = process_client_message(message_from_client)
+            SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
             send_message(client, response)
+            SERVER_LOGGER.debug(f'Соединение с клиентом {client_ip} закрывается.')
             client.close()
-        except (ValueError, json.JSONDecodeError):
-            print('Принято некорретное сообщение от клиента.')
+        except json.JSONDecodeError:
+            SERVER_LOGGER.error(f'Не удалось декодировать JSON строку, полученную от '
+                                f'клиента {client_ip}. Соединение закрывается.')
+            client.close()
+        except IncorrectDataRecivedError:
+            SERVER_LOGGER.error(f'От клиента {client_ip} приняты некорректные данные. '
+                                f'Соединение закрывается.')
             client.close()
 
 
