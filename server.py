aade5dedf3c9fd416b674c6a67a9ecea057bbe87
Demid <bydimko@yandex.ru>
2022-02-07 23:56:25 +0300

    HW_7 update client and server

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 """Программа-сервер"""
 import argparse
+import select
 import socket
 import sys
 import json
 import logging
+import time
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
-    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, IP_ADDRESS, PORT
+    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT, IP_ADDRESS, PORT, MESSAGE, MESSAGE_TEXT, SENDER
 from common.utils import get_message, send_message
 from errors import IncorrectDataRecivedError
 from logs import server_log_config
@@ -15,9 +17,8 @@ from decor import log
 SERVER_LOGGER = logging.getLogger('server')
 
 
-
 @log
-def process_client_message(message):
+def process_client_message(message, messages_list, client):
     '''
     Обработчик сообщений от клиентов, принимает словарь -
     сообщение от клинта, проверяет корректность,
@@ -26,33 +27,32 @@ def process_client_message(message):
     :param message:
     :return:
     '''
+    SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {message}')
     if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
             and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
-        return {RESPONSE: 200}
-    return {
-        RESPONSE: 400,
-        ERROR: 'Bad Request'
-    }
+        send_message(client, {RESPONSE: 200})
+        return
+
+    elif ACTION in message and message[ACTION] == MESSAGE and \
+            TIME in message and MESSAGE_TEXT in message:
+        messages_list.append((message[ACCOUNT_NAME], message[MESSAGE_TEXT]))
+        return
+    else:
+        send_message(client, {
+            RESPONSE: 400,
+            ERROR: 'Bad Request'
+        })
+        return
 
 
 @log
 def create_arg_parser():
     """
     Парсер аргументов коммандной строки
-    :return:
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
     parser.add_argument('-a', default='', nargs='?')
-    return parser
-
-
-def main():
-    '''
-    Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
-    :return:
-    '''
-    parser = create_arg_parser()
     namespace = parser.parse_args(sys.argv[1:])
     listen_address = namespace.a
     listen_port = namespace.p
@@ -61,6 +61,15 @@ def main():
         SERVER_LOGGER.critical(
             f'{listen_port} В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
         sys.exit(1)
+
+    return listen_address, listen_port
+
+
+def main():
+    '''
+    Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию.
+    '''
+    listen_address, listen_port = create_arg_parser()
 
     SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
                        f'адрес с которого принимаются подключения: {listen_address}. '
@@ -78,29 +87,57 @@ def main():
     # Готовим сокет
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transport.bind((listen_address, listen_port))
+    transport.settimeout(0.5)
+
+    clients = []
+    messages = []
 
     # Слушаем порт
     transport.listen(MAX_CONNECTIONS)
 
     while True:
-        client, client_ip = transport.accept()
-        SERVER_LOGGER.info(f'Установлено соедение с ПК {client_ip}')
         try:
-            message_from_client = get_message(client)
-            SERVER_LOGGER.debug(f'Получено сообщение {message_from_client}')
-            response = process_client_message(message_from_client)
-            SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
-            send_message(client, response)
-            SERVER_LOGGER.debug(f'Соединение с клиентом {client_ip} закрывается.')
-            client.close()
-        except json.JSONDecodeError:
-            SERVER_LOGGER.error(f'Не удалось декодировать JSON строку, полученную от '
-                                f'клиента {client_ip}. Соединение закрывается.')
-            client.close()
-        except IncorrectDataRecivedError:
-            SERVER_LOGGER.error(f'От клиента {client_ip} приняты некорректные данные. '
-                                f'Соединение закрывается.')
-            client.close()
+            client, client_ip = transport.accept()
+        except OSError:
+            pass
+        else:
+            SERVER_LOGGER.info(f'Установлено соедение с ПК {client_ip}')
+            clients.append(client)
+
+        recv_data_lst = []
+        send_data_lst = []
+        err_lst = []
+
+        try:
+            if clients:
+                recv_data_lst, send_data_lst, err_lst = select.select(clients, clients, [], 0)
+        except OSError:
+            pass
+
+        if recv_data_lst:
+            for client_with_message in recv_data_lst:
+                try:
+                    process_client_message(get_message(client_with_message),
+                                           messages, client_with_message)
+                except:
+                    SERVER_LOGGER.info(f'Клиент {client_with_message.getpeername()} '
+                                       f'отключился от сервера.')
+                    clients.remove(client_with_message)
+
+        if messages and send_data_lst:
+            message = {
+                ACTION: MESSAGE,
+                SENDER: messages[0][0],
+                TIME: time.time(),
+                MESSAGE_TEXT: messages[0][1]
+            }
+            del messages[0]
+            for waiting_client in send_data_lst:
+                try:
+                    send_message(waiting_client, message)
+                except:
+                    SERVER_LOGGER.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
+                    clients.remove(waiting_client)
 
 
 if __name__ == '__main__':
