84607e8d30aa553367805ae57d5dad383bfaaa5b
Demid <bydimko@yandex.ru>
2022-03-07 17:34:35 +0300

    PyQt_HW6 minor changes

--- logs/client_log_config.py
+++ logs/client_log_config.py
@@ -1,36 +1,32 @@
 import sys
 import os
-import logging
-from common.variables import LOGGING_LEVEL, CLIENT_FORMATTER
 sys.path.append('../')
+import logging
+from common.variables import LOGGING_LEVEL
+
+# создаём формировщик логов (formatter):
+client_formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
 
 # Подготовка имени файла для логирования
-PATH = os.path.dirname(os.path.abspath(__file__))
-PATH = os.path.join(PATH, 'client.log')
+path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(path, 'client.log')
 
-# Создаем обработчик, который выводит сообщения в поток stderr
-STREAM_HANDLER = logging.StreamHandler(sys.stderr)
-STREAM_HANDLER.setLevel(logging.INFO)
+# создаём потоки вывода логов
+steam = logging.StreamHandler(sys.stderr)
+steam.setFormatter(client_formatter)
+steam.setLevel(logging.INFO)
+log_file = logging.FileHandler(path, encoding='utf8')
+log_file.setFormatter(client_formatter)
 
-# Создать обработчик, который выводит сообщения в файл
-LOG_FILE = logging.FileHandler(PATH, encoding='utf8')
-
-# подключаем объект Formatter к обработчикам
-STREAM_HANDLER.setFormatter(CLIENT_FORMATTER)
-LOG_FILE.setFormatter(CLIENT_FORMATTER)
-
-# создаём регистратор
-CLIENT_LOG = logging.getLogger('client')
-
-# Добавляем обработчики к регистратору
-CLIENT_LOG.addHandler(STREAM_HANDLER)
-CLIENT_LOG.addHandler(LOG_FILE)
-CLIENT_LOG.setLevel(LOGGING_LEVEL)
+# создаём регистратор и настраиваем его
+logger = logging.getLogger('client')
+logger.addHandler(steam)
+logger.addHandler(log_file)
+logger.setLevel(LOGGING_LEVEL)
 
 # отладка
 if __name__ == '__main__':
-    CLIENT_LOG.critical('Критическая ошибка')
-    CLIENT_LOG.error('Ошибка')
-    CLIENT_LOG.warning('Предупреждение!')
-    CLIENT_LOG.debug('Отладочная информация')
-    CLIENT_LOG.info('Информационное сообщение')
+    logger.critical('Test critical event')
+    logger.error('Test error ivent')
+    logger.debug('Test debug ivent')
+    logger.info('Test info ivent')
--- logs/server_log_config.py
+++ logs/server_log_config.py
@@ -1,37 +1,33 @@
 import sys
-import os
+sys.path.append('../')
 import logging
 import logging.handlers
-from common.variables import LOGGING_LEVEL, SERVER_FORMATTER
-sys.path.append('../')
+import os
+from common.variables import LOGGING_LEVEL
+
+# создаём формировщик логов (formatter):
+server_formatter = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
 
 # Подготовка имени файла для логирования
-PATH = os.path.dirname(os.path.abspath(__file__))
-PATH = os.path.join(PATH, 'server.log')
+path = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(path, 'server.log')
 
-# Создаем обработчик, который выводит сообщения в поток stderr
-STREAM_HANDLER = logging.StreamHandler(sys.stderr)
-STREAM_HANDLER.setLevel(logging.INFO)
+# создаём потоки вывода логов
+steam = logging.StreamHandler(sys.stderr)
+steam.setFormatter(server_formatter)
+steam.setLevel(logging.INFO)
+log_file = logging.handlers.TimedRotatingFileHandler(path, encoding='utf8', interval=1, when='D')
+log_file.setFormatter(server_formatter)
 
-# Создать обработчик, который выводит сообщения в файл
-LOG_FILE = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf8', interval=1, when='D')
-
-# подключаем объект Formatter к обработчикам
-STREAM_HANDLER.setFormatter(SERVER_FORMATTER)
-LOG_FILE.setFormatter(SERVER_FORMATTER)
-
-# создаём регистратор
-SERVER_LOG = logging.getLogger('server')
-
-# Добавляем обработчики к регистратору
-SERVER_LOG.addHandler(STREAM_HANDLER)
-SERVER_LOG.addHandler(LOG_FILE)
-SERVER_LOG.setLevel(LOGGING_LEVEL)
+# создаём регистратор и настраиваем его
+logger = logging.getLogger('server')
+logger.addHandler(steam)
+logger.addHandler(log_file)
+logger.setLevel(LOGGING_LEVEL)
 
 # отладка
 if __name__ == '__main__':
-    SERVER_LOG.critical('Критическая ошибка')
-    SERVER_LOG.error('Ошибка')
-    SERVER_LOG.warning('Предупреждение!')
-    SERVER_LOG.debug('Отладочная информация')
-    SERVER_LOG.info('Информационное сообщение')
+    logger.critical('Test critical event')
+    logger.error('Test error ivent')
+    logger.debug('Test debug ivent')
+    logger.info('Test info ivent')
